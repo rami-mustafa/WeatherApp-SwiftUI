f5fe8fa6568a702c4c5838c789686339b162edf3
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-08-01 21:53:06 +0200

    Create a project

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Ghaiath Alhereh on 01.08.23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
