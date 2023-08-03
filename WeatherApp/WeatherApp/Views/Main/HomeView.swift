869f301800a1e105f21ede22a8175c98ae3f9dd9
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-08-03 21:53:31 +0200

    Home View

---
+++ WeatherApp/WeatherApp/Views/Main/HomeView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeView.swift
+//  WeatherApp
+//
+//  Created by Ghaiath Alhereh on 03.08.23.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+
+        ZStack{
+            Color.background
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
