a7e13830612ec3c912717df19f9956e6d65bd4e7
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-08-04 22:45:03 +0200

    Home screen

--- WeatherApp/WeatherApp/Views/Main/HomeView.swift
+++ WeatherApp/WeatherApp/Views/Main/HomeView.swift
@@ -9,9 +9,29 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-
+        
         ZStack{
+            // MARK: Background Color
             Color.background
+                .ignoresSafeArea() // To make full screen
+            
+            // MARK: Background Image
+            Image("Background")
+                .resizable()
+                .ignoresSafeArea() // To make full screen
+            
+            
+            // MARK: House Image
+            Image("House")
+                .frame(maxHeight: .infinity , alignment:  .top)
+                .padding(.top, 257)
+            
+            VStack{
+                Text("Montreal")
+                    .font(.largeTitle)
+                
+                Spacer()
+            }
         }
     }
 }
