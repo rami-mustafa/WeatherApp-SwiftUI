b83d93b334997f1576f61520ae8f63a914316ad5
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-08-05 22:45:46 +0200

    AttributedString

--- WeatherApp/WeatherApp/Views/Main/HomeView.swift
+++ WeatherApp/WeatherApp/Views/Main/HomeView.swift
@@ -20,24 +20,51 @@ struct HomeView: View {
                 .resizable()
                 .ignoresSafeArea() // To make full screen
             
-            
             // MARK: House Image
             Image("House")
                 .frame(maxHeight: .infinity , alignment:  .top)
                 .padding(.top, 257)
+
             
             VStack{
                 Text("Montreal")
                     .font(.largeTitle)
                 
+                VStack{
+//                    Text("19°")
+//                        .font(.system(size: 96 , weight: .thin))
+//                        .foregroundColor(.primary)
+//                    +
+//                    Text("\n")
+//                    +
+//                    Text("Mostly Clear")
+//                        .font(.title3.weight(.semibold))
+//                        .foregroundColor(.secondary)
+                    
+                    Text(attributedString)
+
+                    Text("H:24  L: 21")
+                        .font(.title3.weight(.semibold))
+
+                }
                 Spacer()
             }
+            .padding(.top , 51)
         }
+    }
+    
+    private var attributedString: AttributedString {
+        var string = AttributedString("19°" + "\n " + "Mostly Clear")
+        
+       
+        
+        return string
     }
 }
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
