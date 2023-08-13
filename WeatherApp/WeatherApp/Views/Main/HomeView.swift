c51d8ef89f7caeae5cfa41328328c4e9ab4a2590
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-08-13 20:08:55 +0200

    create a Tab Bar

--- WeatherApp/WeatherApp/Views/Main/HomeView.swift
+++ WeatherApp/WeatherApp/Views/Main/HomeView.swift
@@ -10,54 +10,65 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         
-        ZStack{
-            // MARK: Background Color
-            Color.background
-                .ignoresSafeArea() // To make full screen
-            
-            // MARK: Background Image
-            Image("Background")
-                .resizable()
-                .ignoresSafeArea() // To make full screen
-            
-            // MARK: House Image
-            Image("House")
-                .frame(maxHeight: .infinity , alignment:  .top)
-                .padding(.top, 257)
+        NavigationView {
+            ZStack{
+                // MARK: Background Color
+                Color.background
+                    .ignoresSafeArea() // To make full screen
+                
+                // MARK: Background Image
+                Image("Background")
+                    .resizable()
+                    .ignoresSafeArea() // To make full screen
+                
+                // MARK: House Image
+                Image("House")
+                    .frame(maxHeight: .infinity , alignment:  .top)
+                    .padding(.top, 257)
 
-            
-            VStack{
-                Text("Montreal")
-                    .font(.largeTitle)
                 
                 VStack{
-//                    Text("19°")
-//                        .font(.system(size: 96 , weight: .thin))
-//                        .foregroundColor(.primary)
-//                    +
-//                    Text("\n")
-//                    +
-//                    Text("Mostly Clear")
-//                        .font(.title3.weight(.semibold))
-//                        .foregroundColor(.secondary)
+                    Text("Montreal")
+                        .font(.largeTitle)
                     
-                    Text(attributedString)
+                    VStack{
 
-                    Text("H:24  L: 21")
-                        .font(.title3.weight(.semibold))
+                        
+                        Text(attributedString)
 
+                        Text("H:24  L: 21")
+                            .font(.title3.weight(.semibold))
+
+                    }
+                    Spacer()
                 }
-                Spacer()
+                .padding(.top , 51)
+                
+                
+                // MARK: Tab Bar
+                TabBar(action: {})
             }
-            .padding(.top , 51)
         }
     }
     
     private var attributedString: AttributedString {
         var string = AttributedString("19°" + "\n " + "Mostly Clear")
         
-       
+        if let temp = string.range(of: "19°") {
+            string[temp].font = .system(size: 96, weight: .thin)
+            string[temp].foregroundColor = .primary
+        }
         
+        if let pipe = string.range(of: " | "){
+            string[pipe].font = .title3.weight(.semibold)
+            string[pipe].foregroundColor = .secondary
+        }
+        
+        
+        if let weather = string.range(of: "Mostly Clear"){
+            string[weather].font = .title3.weight(.semibold)
+            string[weather].foregroundColor = .secondary
+        }
         return string
     }
 }
