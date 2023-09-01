19688820dcb41db323f70c1e751bc0e896332094
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-09-01 22:39:54 +0200

    Segmented Buttons

--- WeatherApp/WeatherApp/Views/Components/SegmentedControl.swift
+++ WeatherApp/WeatherApp/Views/Components/SegmentedControl.swift
@@ -4,7 +4,33 @@ import SwiftUI
 
 struct SegmentedControl: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 5) {
+            
+            //MARK: Segmented Buttons
+            HStack {
+                Spacer()
+
+                Button {
+                    
+                } label: {
+                    Text("Hourly Forecast")
+                }
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("Weekly Forecast")
+                }
+                
+                Spacer()
+
+            }
+            .font(.subheadline.weight(.semibold))
+            .foregroundColor(.secondary)
+        }
+        .padding(.top , 25)
     }
 }
 
