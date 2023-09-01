617155474819f1333a6589c53e4e26b21497b95b
rami-mustafa <62889691+rami-mustafa@users.noreply.github.com>
2023-09-01 22:35:48 +0200

    SegmentedControl Component

---
+++ WeatherApp/WeatherApp/Views/Components/SegmentedControl.swift
@@ -0,0 +1,15 @@
+
+
+import SwiftUI
+
+struct SegmentedControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SegmentedControl_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentedControl()
+    }
+}
